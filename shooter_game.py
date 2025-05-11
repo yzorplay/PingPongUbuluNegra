6cafc9a3dcf7038b51cf44f741a81de8d0cb095a
yzorplay <125126432+yzorplay@users.noreply.github.com>
2025-05-11 13:39:49 +0300

    Динамика!
    Теперь игра заиграет новыми красками! просто попробуйте на слух эту музыку? слышиите? это игра, или это больничный кардио-стимулятор? следите за своими динамиками!

--- shooter_game.py
+++ shooter_game.py
@@ -1,4 +1,6 @@
 from pygame import *
+import math
+import random
 window = display.set_mode((700, 500))
 display.set_caption('Пинг понг убили негра')
 color = (0, 0, 0)
@@ -6,10 +8,12 @@ window.fill(color)
 font.init()
 final = False
 
-p1s = 0
-p2s = 0
+p1s = 10
+p2s = 10
 winer = "слева"
 luser = "вправа"
+
+
 
 
 f = font.Font(None, 36)
@@ -41,41 +45,108 @@ class Boll():
         global p1s, p2s, final
         if self.y <= 0 or self.y >= 480:
             self.my *= -1
+            sound_generator.play_sound(222, 0.1)
         
-        if self.x >= pl2.x - 20 and self.y >= pl2.y and self.y <= pl2.y + pl2.sy:
+        if self.x >= pl2.x - 20 and self.y >= pl2.y and self.y <= pl2.y + pl2.sy and self.x <= pl2.x:
             self.mx *= -1
             self.mx *= 1.6
             self.my *= 1.6
-        elif self.x <= pl.x + 20 and self.y >= pl.y and self.y <= pl.y + pl.sy:
+            sound_generator.play_sound(333, 0.1)
+        elif self.x <= pl.x + 20 and self.y >= pl.y and self.y <= pl.y + pl.sy and self.x >= pl.x:
             self.mx *= -1
             self.mx *= 1.6
             self.my *= 1.6
+            sound_generator.play_sound(444, 0.1)
 
-        if self.x >= pl2.x + 30:
-            p1s += 1
+        if self.x >= pl2.x + 80:
+            p1s -= 1
             b.x = 350
             b.y = 250
-            b.mx *= -1
-            self.mx = 2
-            self.my = -2
-        elif self.x <= pl.x - 30:
-            p2s += 1
+            #b.mx *= -1
+            self.mx = random.randint(-4, 4)
+            self.my = random.randint(-4, 4)
+            if self.mx == 0 or self.mx == -1 or self.mx == 1:
+                self.mx = random.randint(-4, 4)
+                self.my = random.randint(-4, 4)
+            sound_generator.play_sound(80, 0.1, sound_generator.play_sound, (80, 0.1))
+        elif self.x <= pl.x - 80:
+            p2s -= 1
             b.x = 350
             b.y = 250
-            b.mx *= -1
-            self.mx = 2
-            self.my = -2
+            #b.mx *= -1
+            self.mx = random.randint(-4, 4)
+            self.my = random.randint(-4, 4)
+            if self.mx == 0 or self.mx == -1 or self.mx == 1:
+                self.mx = random.randint(-4, 4)
+                self.my = random.randint(-4, 4)
+            sound_generator.play_sound(80, 0.1, sound_generator.play_sound, (80, 0.1))
         
-        if p1s == 10:
+        if p1s == 0:
             winer = "справо"
             luser = "слева"
             final = True
-        elif p2s == 10:
+        elif p2s == 0:
             winer = "слево"
             luser = "справо"
             final = True
 
 
+
+class PixelSoundGenerator:
+    def __init__(self, sample_rate=44100, volume=0.2, num_channels=8):
+        self.sample_rate = sample_rate
+        self.volume = volume
+        mixer.init(frequency=self.sample_rate, size=-16, channels=1)  # Инициализируем mixer
+        mixer.set_num_channels(num_channels)  # Устанавливаем количество каналов
+        self.channels = [mixer.Channel(i) for i in range(num_channels)]
+        self.channel_callbacks = {} # Словарь для хранения коллбэков для каждого канала
+
+    def generate_pixel_sound(self, frequency, duration):
+        num_samples = int(self.sample_rate * duration)
+        wave_data = []
+        for i in range(num_samples):
+            # Прямоугольная волна: 1, если sin(x) > 0, и -1, если sin(x) < 0
+            sample = self.volume if math.sin(2 * math.pi * frequency * i / self.sample_rate) > 0 else -self.volume
+            # Ограничиваем значения от -1 до 1
+            sample = max(-1, min(1, sample))
+            # Преобразуем значения от -1 до 1 в целые числа в диапазоне -32768 до 32767
+            sample_value = int(sample * 32767)
+            wave_data.append(sample_value)
+
+        # Преобразуем в формат, понятный 
+        sound_array = mixer.Sound(buffer=b''.join([sample.to_bytes(2, byteorder='little', signed=True) for sample in wave_data]))
+        return sound_array
+
+    def play_sound(self, frequency, duration, callback=None, callback_args=()):
+        sound = self.generate_pixel_sound(frequency, duration)
+        channel = self.find_available_channel()
+
+        if channel:
+            channel.play(sound)
+            self.channel_callbacks[channel] = (callback, callback_args) # Сохраняем коллбэк для этого канала
+            return channel
+        else:
+            print("Все каналы заняты!")
+            return None
+
+    def find_available_channel(self):
+        for channel in self.channels:
+            if not channel.get_busy():
+                return channel
+        return None
+
+    def check_finished_channels(self):
+        for channel, (callback, callback_args) in list(self.channel_callbacks.items()):
+            if not channel.get_busy():  # Если канал свободен (звук закончился)
+                if callback:
+                    callback(*callback_args)  # Вызываем коллбэк
+                del self.channel_callbacks[channel]  # Удаляем коллбэк из словаря
+
+    def cleanup(self):
+        mixer.quit()  # Выключаем mixer, когда он больше не нужен
+
+
+sound_generator = PixelSoundGenerator() # Создаем экземпляр класса
 
 
 
@@ -83,8 +154,8 @@ class Boll():
 
 
 b = Boll(350, 250, 2, -2)
-pl = Player(20,-10, 20, 80)
-pl2 = Player(670,-10, 20, 80)
+pl = Player(60,-10, 20, 80)
+pl2 = Player(620,-10, 20, 80)
 
 
 FPS = 60
@@ -92,16 +163,26 @@ clock = time.Clock()
 finish = True
 game = True
 while game:
+    sound_generator.check_finished_channels()
     window.fill(color)
     if not final:
         b.blit()
         b.move()
         pl.blit()
         pl2.blit()
-        text1 = f.render(str(p1s), True, (180, 0, 0))
-        text2 = f.render(str(p2s), True, (180, 0, 0))
-        window.blit(text1, (100, 50))
-        window.blit(text2, (600, 50))
+        #text1 = f.render(str(p1s), True, (180, 0, 0))
+        #text2 = f.render(str(p2s), True, (180, 0, 0))
+        #window.blit(text1, (100, 50))
+        #window.blit(text2, (600, 50))
+
+        for i in range(p2s):
+            rectanos = Rect(5, 480 - i * 20, 50, 10)
+            draw.rect(window, (255, 0, 0), rectanos)
+        for i in range(p1s):
+            rectanos = Rect(645, 480 - i * 20, 50, 10)
+            draw.rect(window, (255, 0, 0), rectanos)
+
+
         keys = key.get_pressed()
     else:
         text3 = f.render("игрок " + winer + " красава, игрок " + luser + " нубский лузер!", True, (180, 0, 0))
